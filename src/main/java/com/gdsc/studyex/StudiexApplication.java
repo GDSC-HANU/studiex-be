eb2de6a849ea3b02ee70c85fb6cbd58460770581
quanvda <quandang2102@gmail.com>
2022-12-10 14:48:33 +0700

    studiex

--- src/main/java/com/gdsc/studyex/StudiexApplication.java
+++ src/main/java/com/gdsc/studyex/StudiexApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyexApplication {
+public class StudiexApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyexApplication.class, args);
+		SpringApplication.run(StudiexApplication.class, args);
 	}
 
 }
