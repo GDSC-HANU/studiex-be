86eb6b41074e0e3d584fb08062a163fca9c404e8
quanvda <quandang2102@gmail.com>
2023-01-07 19:24:41 +0700

    cors

---
+++ src/main/java/com/gdsc/studiex/infrastructure/share/controllers/WebConfig.java
@@ -0,0 +1,16 @@
+package com.gdsc.studiex.infrastructure.share.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
