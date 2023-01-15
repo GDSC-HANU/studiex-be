e0f71847ec48e994cf8670370c99f95d2a7a39de
quanvda <quandang2102@gmail.com>
2023-01-15 18:37:10 +0700

    system update

--- src/main/java/com/gdsc/studiex/infrastructure/share/controllers/SystemController.java
+++ src/main/java/com/gdsc/studiex/infrastructure/share/controllers/SystemController.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 @RestController
 @Slf4j
@@ -25,8 +27,26 @@ public class SystemController {
         final Process process = Runtime.getRuntime().exec(command);
         log.info("Start running command: " + command);
         if (wait) {
+            watch(process);
             process.waitFor();
             log.info("Finish running command: " + command);
         }
     }
+
+    private static void watch(final Process process) {
+        new Thread() {
+            public void run() {
+                BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line = null;
+                try {
+                    while ((line = input.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
 }
