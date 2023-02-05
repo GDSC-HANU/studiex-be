ef777354bb54cdcb1ed829da0aa95d675ff8e21d
Nguyễn Thành Đạt <dat.nguyenthanh@ddsolution.vn>
2023-02-05 18:35:34 +0700

    update api create pair request

--- src/main/java/com/gdsc/studiex/domain/pair/services/CreatePairRequestService.java
+++ src/main/java/com/gdsc/studiex/domain/pair/services/CreatePairRequestService.java
@@ -5,6 +5,9 @@ import com.gdsc.studiex.domain.pair.models.PairRequest;
 import com.gdsc.studiex.domain.pair.models.PairRequestDTO;
 import com.gdsc.studiex.domain.pair.repositories.PairRepository;
 import com.gdsc.studiex.domain.pair.repositories.PairRequestRepository;
+import com.gdsc.studiex.domain.share.exceptions.BusinessLogicException;
+import com.gdsc.studiex.domain.studier.models.Studier;
+import com.gdsc.studiex.domain.studier.repositories.StudierRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +18,19 @@ import java.util.List;
 public class CreatePairRequestService {
     private final PairRequestRepository pairRequestRepository;
     private final PairRepository pairRepository;
+    private final StudierRepository studierRepository;
 
-    public void createPairRequest(PairRequestDTO pairRequestDTO) {
+    public void createPairRequest(PairRequestDTO pairRequestDTO) throws BusinessLogicException {
+        Studier studierToPair = studierRepository.findByStudierId(pairRequestDTO.getToStudierId());
         PairRequest existedPairRequest = pairRequestRepository.findPairRequest(
                 List.of(pairRequestDTO.fromStudierId, pairRequestDTO.toStudierId)
         );
         Pair existedPair = pairRepository.findPair(
                 List.of(pairRequestDTO.fromStudierId, pairRequestDTO.toStudierId)
         );
+        if(studierToPair == null) {
+            throw new BusinessLogicException("Cannot found this studier", "NOT_FOUND");
+        }
         if(existedPair == null && existedPairRequest == null) {
             PairRequest pairRequest = PairRequest.newPairRequestBuilder()
                     .fromStudierId(pairRequestDTO.fromStudierId)
