9412a25f4a2d912cd48fc630ddd21b7c039005a8
Nguyễn Thành Đạt <dat.nguyenthanh@ddsolution.vn>
2023-03-01 17:31:18 +0700

    api update supply & demand

--- src/main/java/com/gdsc/studiex/infrastructure/supply_and_demand/controllers/demand/SaveDemandsController.java
+++ src/main/java/com/gdsc/studiex/infrastructure/supply_and_demand/controllers/demand/SaveDemandsController.java
@@ -3,10 +3,17 @@ package com.gdsc.studiex.infrastructure.supply_and_demand.controllers.demand;
 import com.gdsc.studiex.domain.share.models.Id;
 import com.gdsc.studiex.domain.studier_auth.services.AuthorizeStudierService;
 import com.gdsc.studiex.domain.supply_and_demand.models.demand.DemandsDTO;
+import com.gdsc.studiex.domain.supply_and_demand.models.supply.SuppliesDTO;
 import com.gdsc.studiex.domain.supply_and_demand.services.demand.SaveDemandsService;
 import com.gdsc.studiex.infrastructure.share.controllers.ControllerHandler;
+import com.gdsc.studiex.infrastructure.share.controllers.Socket;
 import com.gdsc.studiex.infrastructure.share.object_mapper.CustomObjectMapper;
+import com.gdsc.studiex.infrastructure.supply_and_demand.controllers.supply.SaveSuppliesController;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +28,15 @@ public class SaveDemandsController {
     private SaveDemandsService saveDemandsService;
     @Autowired
     private AuthorizeStudierService authorizeStudierService;
+    @Autowired
+    private Socket socket;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InputUpdate {
+        public String accessToken;
+        public List<DemandsDTO.DemandDTO> demandDTOs;
+    }
 
     @PostMapping("/supplyAndDemand/demand")
     public ResponseEntity<?> saveSupplies(@RequestHeader(name = "access-token", required = true) String accessToken,
@@ -38,6 +54,23 @@ public class SaveDemandsController {
                     "Success",
                     null
             );
+        });
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void updateSupply() {
+        socket.addEventListener("UPDATE_DEMANDS", String.class, (socketIOClient, s, ackRequest) -> {
+            SaveDemandsController.InputUpdate input = CustomObjectMapper.instance().readValue(
+                    s,
+                    new com.fasterxml.jackson.core.type.TypeReference<SaveDemandsController.InputUpdate>() {
+                    }
+            );
+            Id studierId = authorizeStudierService.authorize(input.accessToken);
+            saveDemandsService.saveDemands(
+                    studierId,
+                    input.demandDTOs
+            );
+            socketIOClient.sendEvent("UPDATE_DEMANDS", "Update successfully");
         });
     }
 }
--- src/main/java/com/gdsc/studiex/infrastructure/supply_and_demand/controllers/supply/SaveSuppliesController.java
+++ src/main/java/com/gdsc/studiex/infrastructure/supply_and_demand/controllers/supply/SaveSuppliesController.java
@@ -2,11 +2,19 @@ package com.gdsc.studiex.infrastructure.supply_and_demand.controllers.supply;
 
 import com.gdsc.studiex.domain.share.models.Id;
 import com.gdsc.studiex.domain.studier_auth.services.AuthorizeStudierService;
+import com.gdsc.studiex.domain.supply_and_demand.models.supply.CustomSupplyItem;
 import com.gdsc.studiex.domain.supply_and_demand.models.supply.SuppliesDTO;
+import com.gdsc.studiex.domain.supply_and_demand.models.supply.SupplyPriority;
 import com.gdsc.studiex.domain.supply_and_demand.services.supply.SaveSuppliesService;
 import com.gdsc.studiex.infrastructure.share.controllers.ControllerHandler;
+import com.gdsc.studiex.infrastructure.share.controllers.Socket;
 import com.gdsc.studiex.infrastructure.share.object_mapper.CustomObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +29,15 @@ public class SaveSuppliesController {
     private SaveSuppliesService saveSuppliesService;
     @Autowired
     private AuthorizeStudierService authorizeStudierService;
+    @Autowired
+    private Socket socket;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InputUpdate {
+        public String accessToken;
+        public List<SuppliesDTO.SupplyDTO> supplyDTOs;
+    }
 
     @PostMapping("/supplyAndDemand/supply")
     public ResponseEntity<?> saveSupplies(@RequestHeader(name = "access-token", required = true) String accessToken,
@@ -37,6 +54,23 @@ public class SaveSuppliesController {
                     "Success",
                     null
             );
+        });
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void updateSupply() {
+        socket.addEventListener("UPDATE_SUPPLIES", String.class, (socketIOClient, s, ackRequest) -> {
+            SaveSuppliesController.InputUpdate input = CustomObjectMapper.instance().readValue(
+                    s,
+                    new com.fasterxml.jackson.core.type.TypeReference<SaveSuppliesController.InputUpdate>() {
+                    }
+            );
+            Id studierId = authorizeStudierService.authorize(input.accessToken);
+            saveSuppliesService.saveSupplies(
+                    studierId,
+                    input.supplyDTOs
+            );
+            socketIOClient.sendEvent("UPDATE_SUPPLIES", "Update successfully");
         });
     }
 }
